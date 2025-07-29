01d85b400f19276b340f2ee64124473903b7adc4
Malcome Fein <mfein90@gmail.com>
2025-07-29 10:56:47 -0400

    add create the game screen changes

--- core/src/main/java/com/mfein/sfs/SFS.java
+++ core/src/main/java/com/mfein/sfs/SFS.java
@@ -1,14 +1,18 @@
 package com.mfein.sfs;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mfein.sfs.resources.Assets;
+import com.mfein.sfs.screens.GameScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class SFS extends ApplicationAdapter {
+public class SFS extends Game {
     public SpriteBatch batch;
     public Assets assets;
+
+    // screens
+    public GameScreen gameScreen;
 
     @Override
     public void create() {
@@ -19,14 +23,15 @@ public class SFS extends ApplicationAdapter {
         // load all assets
         assets.load();
         assets.manager.finishLoading();
+
+        // initialize the game screen and switch to it
+        gameScreen = new GameScreen(this);
+        setScreen(gameScreen);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-
-        batch.end();
+        super.render();
     }
 
     @Override
---
+++ core/src/main/java/com/mfein/sfs/screens/GameScreen.java
@@ -0,0 +1,72 @@
+package com.mfein.sfs.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mfein.sfs.SFS;
+import com.mfein.sfs.resources.Assets;
+
+public class GameScreen implements Screen {
+    private final SFS game;
+
+    // background/ring
+    private Texture backgroundTexture;
+    private Texture frontRopesTexture;
+
+    public GameScreen(SFS game) {
+        this.game = game;
+
+        // create the game area
+        createGameArea();
+    }
+
+    private void createGameArea() {
+        // get the ring textures from the asset manager
+        backgroundTexture = game.assets.manager.get(Assets.BACKGROUND_TEXTURE);
+        frontRopesTexture = game.assets.manager.get(Assets.FRONT_ROPES_TEXTURE);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0,0,0,1);
+
+        // begin drawing
+        game.batch.begin();
+
+        // draw the background
+        game.batch.draw(backgroundTexture, 0,0);
+
+        // end drawing
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
