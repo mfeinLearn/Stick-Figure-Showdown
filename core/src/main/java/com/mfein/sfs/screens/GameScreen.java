cb2ce1c4007c757a2b466d311e905c9db38890e1
Malcome Fein <mfein90@gmail.com>
2025-07-29 12:16:18 -0400

    add Window Size, Cameras & World Units changes

--- core/src/main/java/com/mfein/sfs/screens/GameScreen.java
+++ core/src/main/java/com/mfein/sfs/screens/GameScreen.java
@@ -1,20 +1,31 @@
 package com.mfein.sfs.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mfein.sfs.SFS;
 import com.mfein.sfs.resources.Assets;
+import com.mfein.sfs.resources.GlobalVariables;
 
 public class GameScreen implements Screen {
     private final SFS game;
+
+    private  final OrthographicCamera camera;
 
     // background/ring
     private Texture backgroundTexture;
     private Texture frontRopesTexture;
 
+
     public GameScreen(SFS game) {
         this.game = game;
+
+        // set up the camera
+        camera = new OrthographicCamera(GlobalVariables.WORLD_WIDTH, GlobalVariables.WORLD_HEIGHT);
+        camera.translate(camera.viewportWidth / 2f, camera.viewportHeight /2f);
+        camera.update();
 
         // create the game area
         createGameArea();
@@ -35,11 +46,15 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         ScreenUtils.clear(0,0,0,1);
 
+        // set the sprite batch to use the camera
+        game.batch.setProjectionMatrix(camera.combined );
+
         // begin drawing
         game.batch.begin();
 
         // draw the background
-        game.batch.draw(backgroundTexture, 0,0);
+        game.batch.draw(backgroundTexture, 0,0, backgroundTexture.getWidth() * GlobalVariables.WORLD_SCALE,
+            backgroundTexture.getHeight() *  GlobalVariables.WORLD_SCALE);
 
         // end drawing
         game.batch.end();
