c02355f50145cc3b6e16459c99fc29b55fa84d82
Malcome Fein <mfein90@gmail.com>
2025-08-02 17:43:54 -0400

    add settings screen - draw the widgets changes

--- core/src/main/java/com/mfein/sfs/screens/SettingsScreen.java
+++ core/src/main/java/com/mfein/sfs/screens/SettingsScreen.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -165,9 +167,132 @@ public class SettingsScreen implements Screen {
     }
 
     private void createTables() {
+        // create the main table and add it to the stage
+        Table mainTable = new Table();
+        mainTable.setFillParent(true);
+        mainTable.setRound(false);
+        stage.addActor(mainTable);
 
-        stage.setDebugAll(true);
+        // create the banner table
+        Table bannerTable = new Table();
+        bannerTable.setRound(false);
 
+        // add the back button and the settings image to the banner table
+        bannerTable.add(backButton).size(backButton.getWidth(), backButton.getHeight());
+        bannerTable.add(settingsImage).size(settingsImage.getWidth(), settingsImage.getHeight());
+
+        // add an empty cell to the banner table the same size as the back button in order to center the settings image
+        bannerTable.add().size(backButton.getWidth(), backButton.getHeight());
+
+        // add the banner table to the main table
+        mainTable.add(bannerTable);
+        mainTable.row().padTop(1f);
+
+        // create the audio table
+        Table audioTable = new Table();
+        audioTable.setRound(false);
+
+        // create the music table and set its background to the music setting background image
+        Table musicTable = new Table();
+        musicTable.setRound(false);
+        musicTable.setBackground(musicSettingBackgroundImage.getDrawable());
+        musicTable.setSize(musicSettingBackgroundImage.getWidth(), musicSettingBackgroundImage.getHeight());
+
+        // add an empty cell (for alignment) and the music toggle button to the music table
+        musicTable.add().width(musicSettingBackgroundImage.getWidth() - musicToggleButton.getWidth() - 2f);
+        musicTable.add(musicToggleButton).size(musicToggleButton.getWidth(), musicToggleButton.getHeight());
+
+        // add the music table to the audio table
+        audioTable.add(musicTable).size(musicTable.getWidth(), musicTable.getHeight());
+
+        // create the sounds table and set its background to the sounds setting background image
+        Table soundsTable = new Table();
+        soundsTable.setRound(false);
+        soundsTable.setBackground(soundsSettingBackgroundImage.getDrawable());
+        soundsTable.setSize(soundsSettingBackgroundImage.getWidth(), soundsSettingBackgroundImage.getHeight());
+
+
+        // add an empty cell (for alignment) and the sounds toggle button to the sounds table
+        soundsTable.add().width(soundsSettingBackgroundImage.getWidth() - soundsToggleButton.getWidth() - 2f);
+        soundsTable.add(soundsToggleButton).size(soundsToggleButton.getWidth(), soundsToggleButton.getHeight());
+
+        // add the sounds table to the audio table
+        audioTable.add(soundsTable).size(soundsTable.getWidth(), soundsTable.getHeight());
+
+        // add the audio table to the main table
+        mainTable.add(audioTable);
+        mainTable.row().padTop(1f);
+
+        // create the difficulty table and set its background to the difficulty setting background image
+        Table difficultyTable = new Table();
+        difficultyTable.setRound(false);
+        difficultyTable.setBackground(difficultySettingBackgroundImage.getDrawable());
+        difficultyTable.setSize(difficultySettingBackgroundImage.getWidth(), difficultySettingBackgroundImage.getHeight());
+
+
+        // create the difficulty selection table
+        Table difficultySelectionTable = new Table();
+        difficultySelectionTable.setRound(false);
+
+        // create the difficulty image stack and add the difficulty images to it
+        Stack difficultyImageStack = new Stack();
+        difficultyImageStack.add(easyImage);
+        difficultyImageStack.add(mediumImage);
+        difficultyImageStack.add(hardImage);
+        difficultyImageStack.setSize(easyImage.getWidth(), easyImage.getHeight());
+
+
+        // add the difficulty selection buttons and the difficulty image stack to the difficulty selection table
+        difficultySelectionTable.add(previousDifficultyButton).size(previousDifficultyButton.getWidth(),
+            previousDifficultyButton.getHeight());
+        difficultySelectionTable.add(difficultyImageStack).size(difficultyImageStack.getWidth(),
+            difficultyImageStack.getHeight()).padLeft(0.5f).padRight(0.5f);
+        difficultySelectionTable.add(nextDifficultyButton).size(nextDifficultyButton.getWidth(),
+            nextDifficultyButton.getHeight());
+        difficultySelectionTable.pack();
+
+        // add an empty cell (for alignment) and the difficulty selection table to the difficulty table
+        difficultyTable.add().width(difficultySettingBackgroundImage.getWidth() - difficultySelectionTable.getWidth() - 2f);
+        difficultyTable.add(difficultySelectionTable).size(difficultySelectionTable.getWidth(),
+            difficultySelectionTable.getHeight());
+
+        // add the difficulty table to the main table
+        mainTable.add(difficultyTable).size(difficultyTable.getWidth(), difficultyTable.getHeight());
+        mainTable.row().padTop(1f);
+
+        // create the bottom table
+        Table bottomTable = new Table();
+        bottomTable.setRound(false);
+
+        // create the full screen table and set its background to the full screen setting background image
+        Table fullScreenTable = new Table();
+        fullScreenTable.setRound(false);
+        fullScreenTable.setBackground(fullScreenSettingBackgroundImage.getDrawable());
+        fullScreenTable.setSize(fullScreenSettingBackgroundImage.getWidth(), fullScreenSettingBackgroundImage.getHeight());
+
+        // add an empty cell (for alignment) and the full screen check button to the full screen table
+        fullScreenTable.add().width(fullScreenSettingBackgroundImage.getWidth() - fullScreenCheckButton.getWidth() - 2f);
+        fullScreenTable.add(fullScreenCheckButton).size(fullScreenCheckButton.getWidth(), fullScreenCheckButton.getHeight());
+
+        // add the full screen table to the bottom table
+        bottomTable.add(fullScreenTable).size(fullScreenTable.getWidth(), fullScreenTable.getHeight());
+
+        // create the blood table and set its background to the blood setting background image
+        Table bloodTable = new Table();
+        bloodTable.setRound(false);
+        bloodTable.setBackground(bloodSettingBackgroundImage.getDrawable());
+        bloodTable.setSize(bloodSettingBackgroundImage.getWidth(), bloodSettingBackgroundImage.getHeight());
+
+        // add an empty cell (for alignment) and the blood check button to the blood table
+        bloodTable.add().width(bloodSettingBackgroundImage.getWidth() - bloodCheckButton.getWidth() - 2f);
+        bloodTable.add(bloodCheckButton).size(bloodCheckButton.getWidth(), bloodCheckButton.getHeight());
+
+        // add the blood table to the bottom table
+        bottomTable.add(bloodTable).size(bloodTable.getWidth(), bloodTable.getHeight());
+
+
+        // add the bottom table to the main table
+        mainTable.add(bottomTable);
     }
 
 
